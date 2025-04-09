26ddd5586792452f16e6064953f3590b3816477d
Th-Sanchez <thiago.peluci@sptech.school>
2025-04-09 20:43:16 -0300

    Alterando o java

--- src/main/java/com/databrick/Main.java
+++ src/main/java/com/databrick/Main.java
@@ -1,93 +1,43 @@
 package com.databrick;
 
-import com.databrick.entity.Address;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class Main {
 
     private static final Logger logger = Logger.getLogger(Main.class.getName());
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        Path pathFile = Path.of("database.xlsx");
+        try (InputStream file = Files.newInputStream(pathFile);) {
+            Workbook workbook = new XSSFWorkbook(file); // Instancia o arquivo como um objeto Workbook para podermos manipular as suas informações
+            Sheet sheet = workbook.getSheetAt(0); // Pega a primeira planilha do arquivo
 
-        logger.info("Sistema iniciado com sucesso!");
-        System.out.println();
 
-        Thread.sleep(1000);
-
-        System.out.println("[ " + LocalDateTime.now().format(dtf) + " ] Vamos iniciar a registar as informações no sistema.");
-
-        List<Address> addresses = new ArrayList<>();
-        Character continuar;
-
-        do {
-            try {
-                System.out.println();
-                Thread.sleep(500);
-                System.out.print("Rua: ");
-                Thread.sleep(1000);
-                System.out.println("Haddock Lobo");
-
-                Thread.sleep(500);
-                System.out.print("Número: ");
-                Thread.sleep(1000);
-                System.out.println("595");
-
-                Thread.sleep(500);
-                System.out.print("Bairro: ");
-                Thread.sleep(1000);
-                System.out.println("Cerqueira César");
-
-                Thread.sleep(500);
-                System.out.print("CEP: ");
-                Thread.sleep(1000);
-                System.out.println("01414-001");
-
-                LocalDateTime registerDate = LocalDateTime.now();
-                addresses.add(new Address(registerDate, "Haddock Lobo", 595, "Cerqueira César", "01414-001"));
-
-                System.out.println();
-                System.out.print("[ " + LocalDateTime.now().format(dtf) + " ] Registro concluído. Você deseja cadastrar um novo endereço? (S/N) ");
-                continuar = sc.next().toUpperCase().charAt(0);
-                sc.nextLine();
-            } catch (Exception e) {
-                logger.severe(e.getMessage());
-                break;
+            for (Row row : sheet) {
+                List<String> cellValues = new ArrayList<>(); // Lista onde receberemos os valores das células
+                row.forEach(cell ->
+                        cellValues.add(cell.getStringCellValue())
+                );
             }
 
-        } while (continuar.equals('S') || continuar.equals('Y'));
 
-        System.out.println();
-        System.out.println("[ " + LocalDateTime.now().format(dtf) + " ] As informações foram coletadas com sucesso!");
-        System.out.println("[ " + LocalDateTime.now().format(dtf) + " ] Processando...");
 
-        Thread.sleep(2000);
-        System.out.println();
-
-        System.out.println("[ " + LocalDateTime.now().format(dtf) + " ] Processamento concluído. Dados obtidos: ");
-
-        System.out.println("+----------------------+----------------------+----------------------+----------------------+----------------------+");
-        System.out.printf("| %-20s | %-20s | %-20s | %-20s | %-20s |\n", "Hora do registro", "Rua", "Número", "Bairro", "CEP");
-        System.out.println("+----------------------+----------------------+----------------------+----------------------+----------------------+");
-        for (Address address : addresses) {
-            System.out.printf("| %-20s | %-20s | %-20d | %-20s | %-20s |\n", address.getRegisterDate().format(dtf), address.getRoad(), address.getNumber(), address.getNeighborhood(), address.getZipCode());
-            System.out.println("+----------------------+----------------------+----------------------+----------------------+----------------------+");
+        } catch (Exception e) {
+            logger.severe(e.getMessage());
         }
 
-        System.out.println();
-        Thread.sleep(1000);
-
-        logger.info("Sistema finalizado!");
-        Thread.sleep(1000);
-
-        sc.close();
     }
 }
---
+++ src/main/java/com/databrick/entity/Property.java
@@ -0,0 +1,26 @@
+package com.databrick.entity;
+
+import com.databrick.enums.zoneEnum;
+
+public class Property {
+
+    private Double transactionValueDeclared;
+    private Double referenceMarketValue;
+    private Double proportionalReferenceMarketValue;
+    private String registryOffice;
+    private String propertyRegistration;
+    private String sqlStatus;
+    private Double landAream2;
+    private Double builtAream2;
+    private String cep;
+    private Integer number;
+    private String addressType;
+    private String addressName;
+    private String fullAddress;
+    private String state;
+    private String district;
+    private zoneEnum zone;
+    private String latitude;
+    private String longitude;
+
+}
